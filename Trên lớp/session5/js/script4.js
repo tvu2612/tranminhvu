84293303a624603817beec058292618c4a72ea1f
tvu2612 <tvu16119@gmail.com>
2023-10-05 23:52:56 +0700

    up

---
+++ Trên lớp/session5/js/script4.js
@@ -0,0 +1,16 @@
+window.onload = init;
+
+function init(){
+    document.getElementById("btn1").onclick = changeHeading1;
+}
+function changeHeading1(){
+    const elm = document.getElementById("heading1");
+    elm.textContent = "Hello";
+    
+}
+function changeHeading2(){
+
+}
+function changeParagraph(){
+
+}
